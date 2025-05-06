307ea798a52ff1b95bd0c5fc4346eeab1d573e95
nyanchu-0000 <yuinezu.0110@gmail.com>
2025-05-06 13:45:36 +0900

    相対パスを修正

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => {
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-white flex justify-center items-center z-50">
             <div className="relative w-48 h-48">
                 <Image
-                    src="/dinosaur/logo/logo.png"
+                    src="/logo/logo.png"
                     alt="Loading Logo"
                     fill
                     sizes="string"
