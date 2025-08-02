2b397f465f1d607987eb771d314f85d339ad496e
nyanchu-0000 <yuinezu.0110@gmail.com>
2025-08-02 18:25:59 +0900

    ほぼ完成

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,16 @@
-'use client';
+"use client";
+import React, { useEffect, useState } from "react";
+
 const Footer = () => {
+    const [isHomePage, setIsHomePage] = useState(false);
+
+    useEffect(() => {
+        setIsHomePage(window.location.pathname === "/");
+    }, []);
 
     const handleDinoGuideClick = (e) => {
         e.preventDefault();
+
         window.scrollTo({
             top: 710,
             behavior: "smooth",
@@ -13,24 +21,20 @@ const Footer = () => {
         <footer className="w-full bg-neutral-900 bg-opacity-50 text-slate-400 py-4">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center space-x-8">
                 <a
-                    href="#"
-                    className="hover:text-cyan-400 transition-colors duration-300"
+                    href="/"
+                    className="hover:text-lime-600 transition-colors duration-300"
                 >
                     ホーム
                 </a>
-                <a
-                    href="#"
-                    onClick={handleDinoGuideClick}
-                    className="hover:text-cyan-400 transition-colors duration-300"
-                >
-                    恐竜図鑑
-                </a>
-                <a
-                    href="#"
-                    className="hover:text-cyan-400 transition-colors duration-300"
-                >
-                    利用規約
-                </a>
+                {isHomePage && (
+                    <a
+                        href="#"
+                        onClick={handleDinoGuideClick}
+                        className="hover:text-lime-600 transition-colors duration-300"
+                    >
+                        恐竜図鑑
+                    </a>
+                )}
             </div>
 
             <div className="max-w-7xl mx-auto mt-4 px-4 sm:px-6 lg:px-8 text-center text-sm text-slate-500">
